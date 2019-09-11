8210390f1d301921945e996e7c85e294aaf8e532
Wyatt Lynch (RIT Student) <wal8273@ad.rit.edu>
2019-09-11 13:51:41 -0400

    did some cool server response type stuff

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -17,6 +17,13 @@ const badRequest = (request, response, params) => {
     message: 'This request has the required parameters',
   };
 
+  if(!params.valid || params.valid !== 'true'){
+    responseJSON.message = "Missing valid query parameter set to true";
+    respondJSON.id = 'badRequest';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+  return respondJSON(request, response, 200, responseJSON);
+
 };
 
 const notFound = (request, response) => {
@@ -24,7 +31,7 @@ const notFound = (request, response) => {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
   };
-
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 module.exports = {
